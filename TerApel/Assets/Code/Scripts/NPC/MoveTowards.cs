dfacd94cf5ecd86f01f340f728de76a048d0e343
Karina-Blur <k.blurtsyan@st.hanze.nl>
2024-12-08 23:31:25 +0100

    Nav path

--- TerApel/Assets/Code/Scripts/NPC/MoveTowards.cs
+++ TerApel/Assets/Code/Scripts/NPC/MoveTowards.cs
@@ -5,27 +5,12 @@ using UnityEngine;
 public class MoveTowards : MonoBehaviour
 {
     public GameObject NPC;
-    public GameObject SplineG1;
-    public GameObject SplineG2;
+    public GameObject Spline;
     public float speed;
 
-    private void OnTriggerEnter(Collider other)
+    void Update()
     {
-        NPC.transform.position = Vector3.MoveTowards(NPC.transform.position, SplineG1.transform.position, speed);
-        NPC.transform.rotation = SplineG1.transform.rotation;
-        // Check if the colliding object has a specific tag or component
-        if (other.gameObject.CompareTag("Player"))
-        {
-            SplineG1.SetActive(true);
-        }
-    }
-
-    private void Spline2Active()
-    {
-        {
-            SplineG2.SetActive(true);
-            NPC.transform.position = Vector3.MoveTowards(NPC.transform.position, SplineG2.transform.position, speed);
-            NPC.transform.rotation = SplineG2.transform.rotation;
-        }
+        NPC.transform.position = Vector3.MoveTowards(NPC.transform.position, Spline.transform.position, speed);
+        NPC.transform.rotation = Spline.transform.rotation;
     }
 }
